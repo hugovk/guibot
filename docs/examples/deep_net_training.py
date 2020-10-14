b8937418db33e44f2956716a1b126f806025262f
Plamen Dimitrov <pdimitrov@pevogam.com>
2020-10-14 22:59:15 +0300

    move deep methods into the DL training example
    Let the custom finder inherit from a feature finder instead.

--- docs/examples/deep_net_training.py
+++ docs/examples/deep_net_training.py
@@ -27,7 +27,7 @@ from guibot.config import GlobalConfig
 from guibot.imagelogger import ImageLogger
 from guibot.fileresolver import FileResolver
 from guibot.target import Pattern, Image
-from . import CustomFinder
+from . import DeepFinder
 from guibot.errors import *
 
 
@@ -38,8 +38,7 @@ NEEDLE = Pattern(0)
 HAYSTACK = Image('all_shapes')
 LOGPATH = './tmp/'
 REMOVE_LOGPATH = False
-EPOCHS_PER_STAGE = 10
-TOTAL_STAGES = 10
+EPOCHS = 10
 
 
 # Overall logging setup
@@ -54,25 +53,127 @@ GlobalConfig.image_logging_step_width = 4
 ImageLogger.step = 1
 
 
+def train(model, hyperparams, train_samples, train_targets, epoch, data_filename=None):
+    """
+    Train the neural network model.
+
+    :param str train_samples: filename for the samples dataset
+    :param str train_targets: filename for the targets dataset
+    :param int epoch: number of the current training epoch
+    :param data_filename: file name for storing the trained model (won't store if None)
+    :param data_filename: str or None
+    """
+    # create loader for the data (allowing batches and other extras)
+    import torch
+    data_tensor, target_tensor = torch.load(train_samples), torch.load(train_targets)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if hyperparams["use_cuda"].value else {}
+    from torch.utils.data import TensorDataset
+    train_loader = torch.utils.data.DataLoader(TensorDataset(data_tensor, target_tensor),
+                                               batch_size=hyperparams["batch_size"].value,
+                                               shuffle=True, **kwargs)
+
+    # initialize stochastic gradient descent optimizer for learning
+    import torch.optim as optim
+    optimizer = optim.SGD(model.parameters(),
+                          lr=hyperparams["learning_rate"].value,
+                          momentum=hyperparams["sgd_momentum"].value)
+
+    # set the module in training mode
+    model.train()
+
+    import torch.nn.functional as F
+    # loader iterator returns batches of samples
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if hyperparams["use_cuda"].value:
+            data, target = data.cuda(), target.cuda()
+
+        # main training step
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+
+        # backpropagation happens here
+        loss.backward()
+        # learning happens here
+        optimizer.step()
+
+        # log measurements on each ten batches
+        if batch_idx % hyperparams["log_interval"].value == 0:
+            log.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100 * batch_idx / len(train_loader), loss.data[0]))
+
+    # save the network state if required
+    if data_filename is not None:
+        state_dict = model.state_dict()
+        log.debug("Resulting state dictionary (weights, biases, etc) of the network:\n%s", state_dict)
+        torch.save(state_dict, data_filename)
+
+
+def test(model, hyperparams, test_samples, test_targets):
+    """
+    Test the neural network model.
+
+    :param str test_samples: filename for the samples dataset
+    :param str test_targets: filename for the targets dataset
+    """
+    # create loader for the data (allowing batches and other extras)
+    import torch
+    data_tensor, target_tensor = torch.load(test_samples), torch.load(test_targets)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if hyperparams["use_cuda"].value else {}
+    from torch.utils.data import TensorDataset
+    test_loader = torch.utils.data.DataLoader(TensorDataset(data_tensor, target_tensor),
+                                              batch_size=hyperparams["batch_size"].value,
+                                              shuffle=True, **kwargs)
+
+    # set the module in evaluation mode
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+    import torch.nn.functional as F
+    with torch.no_grad():
+        # loader iterator returns batches of samples
+        for data, target in test_loader:
+            if hyperparams["use_cuda"].value:
+                data, target = data.cuda(), target.cuda()
+
+            # main testing step
+            output = model(data)
+            # accumulate negative log likelihood loss
+            test_loss += F.nll_loss(output, target).data[0]
+            # get the index of the max log-probability
+            pred = output.data.max(1)[1]
+            # calculate accuracy as well
+            correct += pred.eq(target.data).cpu().sum()
+
+    # loss function already averages over batch size
+    test_loss /= len(test_loader)
+    # log measurements - this is the only testing action
+    log.info('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100 * correct / len(test_loader.dataset)))
+
+
 # Main configuration and training steps
-finder = CustomFinder()
+finder = DeepFinder()
 # use this to load pretrained model and train futher
 #import torch
 #weights = torch.load(NEEDLE)
 #finder.net.load_state_dict(weights)
 # use this to configure
 #finder.params["find"]["similarity"].value = 0.7
-#finder.params["deep"]["use_cuda"].value = False
-#finder.params["deep"]["batch_size"].value = 1000
-#finder.params["deep"]["log_interval"].value = 10
-#finder.params["deep"]["learning_rate"].value = 0.01
-#finder.params["deep"]["sgd_momentum"].value = 0.5
-for i in range(EPOCHS_PER_STAGE):
-    # train for N epochs saving the obtained needle pattern at each stage
-    # (which could also be helpful in case the training is interrupted)
-    finder.train(TOTAL_STAGES, 'samples_train.pth', 'targets_train.pth', NEEDLE)
+hyperparams = {}
+hyperparams["batch_size"] = 1000
+hyperparams["log_interval"] = 10
+hyperparams["learning_rate"] = 0.01
+hyperparams["sgd_momentum"] = 0.5
+hyperparams["use_cuda"] = False
+for epoch in range(1, EPOCHS + 1):
+    # train for an epoch saving the obtained needle pattern once done
+    train(finder.model, hyperparams, 'samples_train.pth', 'targets_train.pth', epoch, NEEDLE)
     # test trained network on test samples
-    finder.test('samples_test.pth', 'targets_test.pth')
+    test(finder.model, hyperparams, 'samples_test.pth', 'targets_test.pth')
 
 
 # Test trained network on a single test sample with image logging
